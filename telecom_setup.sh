aee4a0017e8749218076d00d0dd1f56ff690a559
nmicaux <micaux.nicolas@gmail.com>
2022-10-10 16:01:47 +0200

    fedora vs ubuntu

--- telecom_setup.sh
+++ telecom_setup.sh
@@ -1,6 +1,8 @@
 # don't forget VPN +
 ssh nmicaux@gpu6.enst.fr
-pip install requests pandas beautifulsoup4 tqdm wandb torch transformers datasets scikit-learn
+# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116  # On Fedora
+# pip install requests pandas beautifulsoup4 tqdm wandb transformers datasets scikit-learn  # On Fedora
+# pip install requests pandas beautifulsoup4 tqdm wandb torch transformers datasets scikit-learn  # On Ubuntu
 python3 -m wandb login
 git clone https://github.com/NicolasMICAUX/human_guided_attention.git
 cd human_guided_attention/
